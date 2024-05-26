c383273fbc1f204e9e46ddb7dc07a02959aa9b1e
Luis Perugachi <luisprg05@gmail.com>
2024-05-25 19:55:07 -0500

    Creado proyecto base con docker para el cliente y servidor

---
+++ app/client/vite.config.js
@@ -0,0 +1,25 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig, loadEnv } from 'vite';
+import { fileURLToPath, URL } from 'node:url';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ command, mode }) => {
+	const env = loadEnv(mode, process.cwd());
+
+	return {
+		plugins: [react()],
+		resolve: {
+			alias: {
+				'@': fileURLToPath(new URL('./src', import.meta.url)),
+			},
+		},
+		server: {
+			proxy: {
+				'/api': {
+					target: env.VITE_API_URL,
+					rewrite: path => path.replace(/^\/api/, ''),
+				},
+			},
+		},
+	};
+});
