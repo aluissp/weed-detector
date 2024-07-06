f06faf004b6e8a0736dd52f5bc0cc9765d0a13de
Luis Perugachi <luisprg05@gmail.com>
2024-07-06 02:17:00 -0500

    Added model class colors

---
+++ app/client/src/constants/modelClassColors.js
@@ -0,0 +1,9 @@
+import { inputNames } from './inputNames';
+
+export const modelClassColors = Object.freeze({
+	[inputNames.diente_leon]: 'azul',
+	[inputNames.kikuyo]: 'celeste',
+	[inputNames.lengua_vaca]: 'blanco',
+	[inputNames.otro]: 'verde',
+	[inputNames.papa]: 'negro',
+});
