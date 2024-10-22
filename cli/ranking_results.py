211191cb405b922238437cee3b9a687db3a53abe
Luis Perugachi <luisprg05@gmail.com>
2024-10-21 22:11:40 -0500

    Upgraded cli/ranking_results.py to generate best ranking sheets

--- cli/ranking_results.py
+++ cli/ranking_results.py
@@ -22,6 +22,14 @@ def parser():
         '-p', '--path', required=True,
         help='Path to the train results folder.'
     )
+    ap.add_argument(
+        '-o', '--output', type=str,
+        help='Path to the output folder to save the ranking results.'
+    )
+    ap.add_argument(
+        '--excel', action='store_true',
+        help='Read only excel files mode.'
+    )
 
     return ap
 
@@ -41,48 +49,83 @@ class RankingResults:
 
         # CLI arguments
         self.path = os.path.realpath(args['path'])
+        self.output = os.path.realpath(args['output']) if args['output'] else self.path
+        self.read_excel = args['excel']
 
         # Valid files
-        valid_extensions = ('.json',)
+        valid_extensions = ('.xlsx',) if self.read_excel else ('.json',)
         self.filenames = list(paths.list_files(self.path, validExts=valid_extensions))
 
         # Results
-        self.best_metrics = pd.DataFrame()
-        self.last_metrics = pd.DataFrame()
-        self.metrics = {}
+        self.metrics_names = ['best_metrics', 'last_metrics']
+        setattr(self, self.metrics_names[0], pd.DataFrame())
+        setattr(self, self.metrics_names[1], pd.DataFrame())
 
-    def load_metrics(self):
-        '''Load the metrics from the train results folder.
-        '''
+        # Default values
+        self.excel_filename = 'ranking.xlsx'
+        self.sheet_names = ['best-mAP50-ranking', 'last-mAP50-ranking']
 
+    def load_json_files(self):
+        '''Load the metrics from the train results folder. '''
         for filename in self.filenames:
-
-            if filename.endswith('metrics.json'):
-                continue
 
             with open(filename, 'r') as file:
                 data = json.load(file)
 
-            for key in ['best_metrics', 'last_metrics']:
+            if 'metrics' not in data:
+                continue
 
-                data['metrics'][key]['path'] = data['metrics']['path']
+            for metric in self.metrics_names:
 
-                data_df = pd.DataFrame([data['metrics'][key]])
+                if metric not in data['metrics']:
+                    continue
 
-                setattr(self, key, pd.concat([getattr(self, key), data_df], ignore_index=True))
+                data['metrics'][metric]['path'] = data['metrics']['path']
+
+                data_df = pd.DataFrame([data['metrics'][metric]])
+
+                setattr(self, metric, pd.concat([getattr(self, metric), data_df], ignore_index=True))
+
+    def load_excel_files(self):
+        '''Load excel files from the train results folder.'''
+
+        for filename in self.filenames:
+
+            for metric, sheet in zip(self.metrics_names, self.sheet_names):
+
+                df = pd.read_excel(filename, sheet_name=sheet)
+
+                setattr(self, metric, pd.concat([getattr(self, metric), df], ignore_index=True))
 
     def save_results(self):
         '''Save the best metrics in a json/excel files.
         '''
+        is_name_ok = False
+        excel_filename = ''
+
+        while not is_name_ok:
+            # Ask for the excel filename
+            excel_filename = input('Enter the excel filename: ')
+            excel_filename = self.excel_filename if excel_filename == '' else excel_filename
+            excel_filename = excel_filename if excel_filename.endswith('.xlsx') else f'{excel_filename}.xlsx'
+
+            print(f'Excel filename: {excel_filename}')
+
+            is_name_ok = input('Is the name ok? (y/n): ').lower() == 'y'
+
+        # Check if the output folder exists
+        if not os.path.exists(self.output):
+            os.makedirs(self.output)
 
         # Save ranking in excel file
-        file_path = os.path.join(self.path, 'ranking.xlsx')
+        file_path = os.path.join(self.output, excel_filename)
 
         with pd.ExcelWriter(file_path) as writer:
 
-            for name in ['best_metrics', 'last_metrics']:
+            summary_df = pd.DataFrame()
+            summary_df.to_excel(writer, sheet_name='summary', index=False)
 
-                self.metrics[name] = {}
+            for name in self.metrics_names:
 
                 df = getattr(self, name)
 
@@ -91,22 +134,33 @@ class RankingResults:
                     # Sort and get best metric
                     df.sort_values(by=key, ascending=False, inplace=True)
 
-                    self.metrics[name][key] = df.iloc[0].to_dict()
+                    row = {
+                        'metric-name': name,
+                        'metric-sorted': key,
+                        **df.iloc[0].to_dict()
+                    }
+
+                    summary_df = pd.concat([summary_df, pd.DataFrame([row])], ignore_index=True)
 
                     # Save in excel file
                     key = key.replace('metrics/', '').replace('(B)', '')
                     df.to_excel(writer, sheet_name=f'{name.replace('_metrics', '')}-{key}-ranking', index=False)
 
-        # Save best metrics in json file
-        file_path = os.path.join(self.path, 'metrics.json')
-
-        with open(file_path, 'w') as file:
-            json.dump(self.metrics, file, indent=4)
+            summary_df.to_excel(writer, sheet_name='summary', index=False)
 
     def run(self):
 
-        # Sort the metrics
-        self.load_metrics()
+        # Check if files exist
+        if not self.filenames:
+            extension = '.xlsx' if self.read_excel else '.json'
+            print(f'[WARNING] It looks like there are no files with {extension} extension.')
+            return
+
+        # Load files
+        if self.read_excel:
+            self.load_excel_files()
+        else:
+            self.load_json_files()
 
         # Save the results
         self.save_results()
